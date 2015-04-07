109bbea24cf4813f4314853ad2de0d994f4e86a4
JohnnyHao <529476075@qq.com>
2015-04-07 11:04:29 +0800

    research

--- templatestudy.py
+++ templatestudy.py
@@ -1,1 +1,29 @@
-__author__ = 'mll-001'
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os.path
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.options
+
+from tornado.options import define, options
+define("port", default=8800, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        lst = ["python","www.itdiffer.com","qiwsir@gmail.com"]  #定义一个list
+        self.render("templatestudy.html", info=lst)                     #将上述定义的list传给模板
+
+handlers = [(r"/", IndexHandler),]
+
+template_path = os.path.join(os.path.dirname(__file__), "template")  #模板路径
+
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers,template_path)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
